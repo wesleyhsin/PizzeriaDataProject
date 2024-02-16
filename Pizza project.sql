5e8ea6d8124f2609510b15b01b36917d166fc1b8
Wesley Hsin <156386124+wesleyhsin@users.noreply.github.com>
2024-02-16 13:53:51 -0800

    Update Pizza project.sql

--- Pizza project.sql
+++ Pizza project.sql
@@ -110,7 +110,7 @@ SELECT
     sh.end_time,
     ((hour(timediff(sh.end_time,sh.start_time))*60)+(minute(timediff(sh.end_time,sh.start_time))))/60 as hours_in_shift,
 	((hour(timediff(sh.end_time,sh.start_time))*60)+(minute(timediff(sh.end_time,sh.start_time))))/60 * s.hourly_rate as staff_cost
-FROM rota r
+FROM rota2 r
 LEFT JOIN staff s on r.staff_id = s.staff_id
 LEFT JOIN shift sh on r.shift_id = sh.shift_id
 
